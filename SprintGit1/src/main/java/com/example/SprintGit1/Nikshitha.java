30dc55f3db8b93d84f986149277bb0b3a434d681
nikshithareddy <nikshithaaella123@gmail.com>
2022-12-05 16:46:05 +0530

    Nikki

---
+++ SprintGit1/src/main/java/com/example/SprintGit1/Nikshitha.java
@@ -0,0 +1,11 @@
+package com.example.SprintGit1;
+
+public class Nikshitha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello iam Nikshitha");
+
+	}
+
+}
