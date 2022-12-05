9f2d2be2df3271531a2a5f04366797c8541b3143
shivani <shivanichowdarapu@gmail.com>
2022-12-05 16:22:58 +0530

    maincommit

---
+++ SprintGit1/src/test/java/com/example/SprintGit1/SprintGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SprintGit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintGit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
